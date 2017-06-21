5eb03080a2fabdd72f5bf80984113c26f73c84da
ezeportela <eportelab@gmail.com>
2017-06-20 22:06:02 -0300

    Step 8

--- client/imports/app/app.module.ts
+++ client/imports/app/app.module.ts
@@ -3,6 +3,7 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MomentModule } from 'angular2-moment';
 import { ChatsPage } from '../pages/chats/chats';
 import { ChatsOptionsComponent } from '../pages/chats/chats-options';
+import { NewChatComponent } from '../pages/chats/new-chat';
 import { PhoneService } from '../services/phone';
 import { MessagesPage } from '../pages/messages/messages';
 import { MyApp } from './app.component';
@@ -18,7 +19,8 @@ import { ProfilePage } from '../pages/profile/profile';
     LoginPage,
     VerificationPage,
     ProfilePage,
-    ChatsOptionsComponent
+    ChatsOptionsComponent,
+    NewChatComponent
   ],
   imports: [
     IonicModule.forRoot(MyApp),
@@ -32,7 +34,8 @@ import { ProfilePage } from '../pages/profile/profile';
     LoginPage,
     VerificationPage,
     ProfilePage,
-    ChatsOptionsComponent
+    ChatsOptionsComponent,
+    NewChatComponent
   ],
   providers: [
     { provide: ErrorHandler, useClass: IonicErrorHandler },
--- server/methods.ts
+++ server/methods.ts
@@ -3,6 +3,35 @@ import { Chats, Messages } from '../imports/collections';
 import { MessageType, Profile } from '../imports/models';
 
 Meteor.methods({
+  addChat(receiverId: string): void {
+    if (!this.userId) {
+      throw new Meteor.Error('unauthorized',
+        'User must be logged-in to create a new chat');
+    }
+
+    check(receiverId, String);
+
+    if (receiverId === this.userId) {
+      throw new Meteor.Error('illegal-receiver',
+        'Receiver must be different than the current logged in user');
+    }
+
+    const chatExists = !!Chats.collection.find({
+      memberIds: { $all: [this.userId, receiverId] }
+    }).count();
+
+    if (chatExists) {
+      throw new Meteor.Error('chat-exists',
+        'Chat already exists');
+    }
+
+    const chat = {
+      memberIds: [this.userId, receiverId]
+    };
+
+    Chats.insert(chat);
+  },
+
   updateProfile(profile: Profile): void {
     if (!this.userId) throw new Meteor.Error('unauthorized',
       'User must be logged-in to create a new chat');
